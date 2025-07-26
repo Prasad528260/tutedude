94146bf8461c2d455d99008683ebd96148082c55
Prasad528260 <prasadsubhedar5460@gmail.com>
2025-07-26 16:50:33 +0530

    Updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,30 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import { PORT } from "./constants/constants.js";
-
+import analyticsRouter from "./routes/analyticsRouter.js";
+import orderRouter from "./routes/orderRouter.js";
+import productRouter from "./routes/productRouter.js";
+import shopRouter from "./routes/shopRouter.js";
+import vendorRouter from "./routes/vendorRouter.js";
+import authRouter from "./routes/authRouter.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+
+app.use("/api/auth", authRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/products", productRouter);
+app.use("/api/shops", shopRouter);
+app.use("/api/vendors", vendorRouter);
+app.use("/api/analytics", analyticsRouter);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
